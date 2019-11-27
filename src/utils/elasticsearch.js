e4aa47b2bf2aaf7851440da55deeaaa7147d775f
Sufiyan Rahmat <sufiyan@incube8.sg>
2019-11-27 17:10:19 +0800

    Added createIndex function in elasticsearch util

--- src/utils/elasticsearch.js
+++ src/utils/elasticsearch.js
@@ -1,8 +1,18 @@
 import config from 'Config/elasticsearch'; // eslint-disable-line import/no-unresolved
 import ElasticsearchService from '../services/ElasticsearchService';
+import LesgoException from '../exceptions/LesgoException';
 
+/**
+ * Reusable instance
+ */
 const singleton = [];
 
+/**
+ * Instantiate the ElasticSearch Service
+ *
+ * @param {object} conn The connection options
+ * @return {object} Returns the driver
+ */
 const es = (conn = null) => {
   if (singleton[conn]) {
     return singleton[conn];
@@ -17,4 +27,26 @@ const es = (conn = null) => {
   return instance;
 };
 
-export default es;
+/**
+ * Create ES index
+ *
+ * @param {object} options The index options
+ * @param {string} indexName Name of the index
+ */
+const createIndex = async (options, indexName = null) => {
+  try {
+    return await es().createIndices(options, indexName);
+  } catch (err) {
+    throw new LesgoException(
+      err.message,
+      'ELASTICSEARCH_INDEX_CREATE_EXCEPTION',
+      500,
+      err
+    );
+  }
+};
+
+export default {
+  es,
+  createIndex,
+};
