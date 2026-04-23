ae9bcaaf4d88dceefdb7becc0eaa6dbb51a44f9e
Anjo Aduana <ojsk8ena@gmail.com>
2026-04-23 17:03:47 +0800

    merge latest 4.1.1 and copy all fix redis from leo

--- dist/services/ElastiCacheRedisService/getElastiCacheRedisClient.d.ts
+++ dist/services/ElastiCacheRedisService/getElastiCacheRedisClient.d.ts
@@ -1,12 +1,15 @@
-import { Cluster } from 'ioredis';
+import { Cluster, ClusterOptions } from 'ioredis';
 import { ClientOptions } from '../../types/aws';
 export interface Singleton {
-    [key: string]: Cluster;
+  [key: string]: Cluster;
 }
 export declare const singleton: Singleton;
 export interface ElastiCacheRedisClientOptions extends ClientOptions {
-    endpoint?: string;
-    port?: number;
+  endpoint?: string;
+  port?: number;
+  clusterOptions?: ClusterOptions;
 }
-declare const getElastiCacheRedisClient: (clientOpts?: ElastiCacheRedisClientOptions) => Promise<Cluster>;
+declare const getElastiCacheRedisClient: (
+  clientOpts?: ElastiCacheRedisClientOptions
+) => Promise<Cluster>;
 export default getElastiCacheRedisClient;
--- dist/types/aws.d.ts
+++ dist/types/aws.d.ts
@@ -1,8 +1,14 @@
+import { ClusterOptions } from 'ioredis';
 export interface ClientOptions {
-    region?: string;
-    singletonConn?: string;
+  region?: string;
+  singletonConn?: string;
+}
+export interface ElastiCacheRedisClientOptions extends ClientOptions {
+  region?: string;
+  singletonConn?: string;
+  clusterOptions?: ClusterOptions;
 }
 export interface RDSAuroraMySQLProxyClientOptions extends ClientOptions {
-    dbCredentialsSecretId?: string;
-    databaseName?: string;
+  dbCredentialsSecretId?: string;
+  databaseName?: string;
 }
